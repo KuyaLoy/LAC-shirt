3c15442eae134a7d49bd9cac62293042dd8c8fd7
Robin Tapiru <robintapiru0894@gmail.com>
2023-07-12 18:56:28 +0400

    fixed code

--- src/components/molecules/CardTshirtHolder/CardTshirtHolder.jsx
+++ src/components/molecules/CardTshirtHolder/CardTshirtHolder.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./CardTshirtHolders.scss";
+import { useNavigate } from "react-router-dom";
 
 const CardTshirtHolder = ({ data }) => {
   const [isHovered, setIsHovered] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
+  const [activeColor, setActiveColor] = useState("black");
+  const [activeImage, setActiveImage] = useState(data.frame_path);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const handleResize = () => {
@@ -28,27 +32,50 @@ const CardTshirtHolder = ({ data }) => {
     }
   };
 
+  const handleClickColor = (color, imagePath) => {
+    setActiveColor(color);
+    setActiveImage(imagePath);
+  };
+
+  const handleClickFrame = () => {
+    navigate(`/details/${data.category}/${data.id}`);
+  };
+
   return (
-    <a
-      href={`/details/${data.category}/${data.id}`}
-      className="frameWrapper"
+    <div
+      className={`frameWrapper ${isMobile ? "mobile" : ""}`}
+      onClick={isMobile ? undefined : handleClickFrame}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <div className="frame">
-        {isHovered ? (
-          <img src={data.frame_gif} alt="" />
-        ) : (
-          <>
-            <img src={data.frame_path} alt="" />
-          </>
-        )}
-
+      <div
+        className={`frame ${isHovered && !isMobile ? "hovered" : ""}`}
+        onClick={isMobile ? handleClickFrame : undefined}
+      >
+        <img
+          src={isHovered && !isMobile ? data.frame_gif : activeImage}
+          alt=""
+        />
         <h2>{`${data.category} ${data.name}`}</h2>
       </div>
       <div className="ColorBox">
-        <span></span>
-        <span></span>
+        <span
+          className={`white ${
+            isMobile && activeColor === "white" ? "active" : ""
+          }`}
+          onClick={() =>
+            handleClickColor(
+              "white",
+              data.attributes.shirt_image.color.white_image_path[1]
+            )
+          }
+        ></span>
+        <span
+          className={`black ${
+            isMobile && activeColor === "black" ? "active" : ""
+          }`}
+          onClick={() => handleClickColor("black", data.frame_path)}
+        ></span>
       </div>
       <div className="Price">{data.price}</div>
       <div className="Sizes">
@@ -56,7 +83,7 @@ const CardTshirtHolder = ({ data }) => {
           <p key={index}> {size}</p>
         ))}
       </div>
-    </a>
+    </div>
   );
 };
 
